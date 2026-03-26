908afbe1fd8cb16380c43bfd6da2a98145b0cf5a
L184899 <laaajo@gmail.com>
2026-03-26 18:51:13 +0100

    flyttet passord ut av main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,40 +2,28 @@ package main;
 
 import entity.Ansatt;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import util.Passwords;
-
-import java.util.Map;
+import util.JPAUtil;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = null;
         EntityManager em = null;
 
         try {
-            emf = Persistence.createEntityManagerFactory(
-                    "oblig3PU",
-                    Map.of("jakarta.persistence.jdbc.password", Passwords.AZURE_PASSWORD)
-            );
+            em = JPAUtil.getEntityManagerFactory().createEntityManager();
 
-            em = emf.createEntityManager();
-
-            Ansatt ansatt = em.find(Ansatt.class, 1);
+            Ansatt ansatt = em.find(Ansatt.class, 1); //l
 
             if (ansatt != null) {
-                System.out.println("Fant ansatt:");
                 System.out.println(ansatt);
             } else {
                 System.out.println("Fant ingen ansatt med id 1.");
             }
 
         } catch (Exception e) {
-            System.out.println("Klarte ikke å hente ansatt.");
             e.printStackTrace();
         } finally {
             if (em != null) em.close();
-            if (emf != null) emf.close();
+            JPAUtil.close();
         }
     }
 }
---
+++ src/main/java/util/JPAUtil.java
@@ -0,0 +1,25 @@
+package util;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Map;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory(
+                    "oblig3PU",
+                    Map.of("jakarta.persistence.jdbc.password", Passwords.AZURE_PASSWORD)
+            );
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return emf;
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+}
